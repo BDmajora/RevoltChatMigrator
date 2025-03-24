cb732142701ad63709d0b4316c3d7fa26803f697
bdmajora <bdmajora@protonmail.com>
2025-03-23 18:02:09 -0600

    Moved project to github

--- app/util/regex.ts
+++
@@ -1,6 +0,0 @@
-export const DiscordEmojiPattern = /<(:|a:)(?<name>.+?):(?<id>[0-9]{1,22})>/g;
-export const DiscordPingPattern = /<(@|@!)(?<id>[0-9]{1,22})>/g;
-export const DiscordChannelPattern = /<#(?<id>[0-9]{1,22})>/g;
-
-export const RevoltPingPattern = /<@(?<id>[0-Z]{1,26})>/g;
-export const RevoltChannelPattern = /<#(?<id>[0-Z]{1,26})>/g;
